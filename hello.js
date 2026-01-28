9c62c07d631d6290a68347a9a94689f4adfbaa53
mvaagsbo <mvaagsbo@gmail.com>
2026-01-28 23:38:22 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
 console.log("Hei Solana! Dette er en test-fil.");
-
+// Ekstra test: Hei fra Norge! Bruker denne repoen til Solana dev-eksperimenter.
+console.log("Aktivitet oppdatert:", new Date().toLocaleDateString());
